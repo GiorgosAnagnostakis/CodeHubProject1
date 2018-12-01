3a4d01bb233aaa85856ae9e571365987cdd43eb1
Anagnostakis <g.anagnostakis@hotmail.com>
2018-12-01 12:10:32 +0200

    Merge branch 'master' of C:\Users\Anagnostakis with conflicts.

--- Desktop/CodeHub/Final Assignment 1/src/Vehicle.java
+++ Desktop/CodeHub/Final Assignment 1/src/Vehicle.java
@@ -1,4 +1,4 @@
-public class Vehicles  {
+public class Vehicle {
 
     String ownerName;
     String plateNumber;
@@ -6,7 +6,7 @@ public class Vehicles  {
     String finishDayInsu;
 
 
-    public Vehicles(String ownerName, String plateNumber, String carType, String finishDayInsu) {
+    public Vehicle(String ownerName, String plateNumber, String carType, String finishDayInsu) {
         this.ownerName = ownerName;
         this.plateNumber = plateNumber;
         this.carType = carType;
@@ -18,7 +18,7 @@ public class Vehicles  {
         return plateNumber;
     }
 
-    /* public int Compare(Vehicles d, Vehicles d1) {
+    /* public int Compare(Vehicle d, Vehicle d1) {
          return d.getPlateNumber().compareTo(d1.getPlateNumber());
      }
  */
